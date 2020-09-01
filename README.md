bff75b5f1403d1a56e57024c6f3a02cdae11bc46
LeenSamZiv <1179364851@qq.com>
2020-09-01 11:27:40 +0800

    🎉 init

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
+- [原始仓库](https://github.com/hhjjj1010/cordova-plugin-alipay-v2)
+- [修复仓库](https://github.com/JayzeeHuang/cordova-plugin-alipay-v2)
 - [README](http://192.168.3.168:12000/tool/docs/tree/dev/ionic%20capacitor%20%E4%B8%8E%20cordova-plugin-alipay-v2%E5%85%BC%E5%AE%B9%E6%80%A7%E9%97%AE%E9%A2%98)
+
+> 2020/09/01
+
+> `原始仓库`插件存在 `UIWebView` 废弃拒审问题
+```
+Dear Developer,
+
+We identified one or more issues with a recent delivery for your app, "动物身份" 1.0.1 (3). Please correct the following issues, then upload again.
+
+ITMS-90809: Deprecated API Usage - New apps that use UIWebView are no longer accepted. Instead, use WKWebView for improved security and reliability. Learn more (https://developer.apple.com/documentation/uikit/uiwebview).
+
+Best regards,
+
+The App Store Team
+```
+> `修复仓库` 存在获取APP_ID方法以及拼接问题
---
+++ src/ios/alipay.m
@@ -0,0 +1,80 @@
+/********* alipay.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+#import <AlipaySDK/AlipaySDK.h>
+
+@interface alipay : CDVPlugin
+@property(nonatomic,strong)NSString *app_id;
+@property(nonatomic,strong)NSString *currentCallbackId;
+
+- (void)payment:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation alipay
+
+#pragma mark "API"
+- (void)pluginInitialize {
+    CDVViewController *viewController = (CDVViewController *)self.viewController;
+    self.app_id = [[self.commandDelegate settings] objectForKey:@"alipayid"];
+}
+
+- (void)payment:(CDVInvokedUrlCommand*)command
+{
+    self.currentCallbackId = command.callbackId;
+    NSString* orderString = [command.arguments objectAtIndex:0];
+    NSString* appScheme = [NSString stringWithFormat:@"ali%@", self.app_id];
+
+    if (orderString != nil) {
+        [[AlipaySDK defaultService] payOrder:orderString fromScheme:appScheme callback:^(NSDictionary *resultDic) {
+            if ([[resultDic objectForKey:@"resultStatus"]  isEqual: @"9000"]) {
+                [self successWithCallbackID:self.currentCallbackId messageAsDictionary:resultDic];
+            } else {
+                [self failWithCallbackID:self.currentCallbackId messageAsDictionary:resultDic];
+            }
+
+            NSLog(@"reslut = %@",resultDic);
+        }];
+
+    }
+}
+
+- (void)handleOpenURL:(NSNotification *)notification
+{
+    NSURL* url = [notification object];
+
+    if ([url isKindOfClass:[NSURL class]] && [url.scheme isEqualToString:[NSString stringWithFormat:@"ali%@", self.app_id]])
+    {
+        [[AlipaySDK defaultService] processOrderWithPaymentResult:url standbyCallback:^(NSDictionary *resultDic) {
+            if ([[resultDic objectForKey:@"resultStatus"]  isEqual: @"9000"]) {
+                [self successWithCallbackID:self.currentCallbackId messageAsDictionary:resultDic];
+            } else {
+                [self failWithCallbackID:self.currentCallbackId messageAsDictionary:resultDic];
+            }
+        }];
+    }
+}
+
+- (void)successWithCallbackID:(NSString *)callbackID withMessage:(NSString *)message
+{
+    CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:message];
+    [self.commandDelegate sendPluginResult:commandResult callbackId:callbackID];
+}
+
+- (void)failWithCallbackID:(NSString *)callbackID withMessage:(NSString *)message
+{
+    CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:message];
+    [self.commandDelegate sendPluginResult:commandResult callbackId:callbackID];
+}
+- (void)successWithCallbackID:(NSString *)callbackID messageAsDictionary:(NSDictionary *)message
+{
+    CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:message];
+    [self.commandDelegate sendPluginResult:commandResult callbackId:callbackID];
+}
+
+- (void)failWithCallbackID:(NSString *)callbackID messageAsDictionary:(NSDictionary *)message
+{
+    CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:message];
+    [self.commandDelegate sendPluginResult:commandResult callbackId:callbackID];
+}
+
+@end
